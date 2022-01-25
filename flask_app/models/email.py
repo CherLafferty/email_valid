6758f605d956d4b3f721348940d2e78a33c2754b
Cher Lafferty <cherlafferty01@gmail.com>
2022-01-25 05:33:07 -0800

    correction

--- flask_app/models/email.py
+++ flask_app/models/email.py
@@ -13,34 +13,34 @@ class Email:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
-@classmethod
-def save(cls,data):
-    query = "INSERT INTO emails (email) VALUES (%(email)s);"
-    return connectToMySQL(cls.db).querey_db(query,data)
+    @classmethod
+    def save(cls,data):
+        query = "INSERT INTO emails (email) VALUES (%(email)s);"
+        return connectToMySQL(cls.db).query_db(query,data)
 
-@classmethod
-def get_all(cls):
-    query= "SELECT * FROM emails;"
-    results = connectToMySQL(cls.db).query_db(query)
-    emails = []
-    for row in results:
-        emails.append( cls(row) )
-    return emails
+    @classmethod
+    def get_all(cls):
+        query= "SELECT * FROM emails;"
+        results = connectToMySQL(cls.db).query_db(query)
+        emails = []
+        for row in results:
+            emails.append( cls(row) )
+        return emails
 
-@classmethod
-def destroy(cls,data):
-    query = "DELETE FROM emails WHERE id = %(id)s;"
-    return connectToMySQL(cls.db).query_db(query,data)
+    @classmethod
+    def destroy(cls,data):
+        query = "DELETE FROM emails WHERE id = %(id)s;"
+        return connectToMySQL(cls.db).query_db(query,data)
 
-@staticmethod
-def is_valid(email):
-    is_valid = True
-    query = "SELECT * FROM emails WHERE email = %(email)s;"
-    results = connectToMySQL(Email.db).query_db(query,email)
-    if len(results) >= 1:
-        flash("Email already taken")
-        is_valid=False
-    if not EMAIL_REGEX.match(email['email']):
-        flash("Invalid Email!")
-        is_valid=False
-    return is_valid
+    @staticmethod
+    def is_valid(email):
+        is_valid = True
+        query = "SELECT * FROM emails WHERE email = %(email)s;"
+        results = connectToMySQL(Email.db).query_db(query,email)
+        if len(results) >= 1:
+            flash("Email already taken")
+            is_valid=False
+        if not EMAIL_REGEX.match(email['email']):
+            flash("Invalid Email!")
+            is_valid=False
+        return is_valid
